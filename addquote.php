e421c6ff4a5bc4f22e3521679d7931056a478a93
Maarten <mawthoonen@gmail.com>
2020-04-03 15:59:25 +0200

    php toegevoegd vet eng

---
+++ addquote.php
@@ -0,0 +1,9 @@
+<?php
+$db = mysqli_connect("130.89.149.254","dhcdrfe180_quoteform","m#&AmXk*_S&[","huizehigh5_quotes")or die('Er kan geen verbinding met de database worden gemaakt.');
+
+$sql = mysqli_query()
+
+echo "succes!"
+?>
+<!--?
+echo '<meta http-equiv=REFRESH CONTENT=0;url='huizehighfive.calslaan.nl/quotes'>';?-->
