ecae6eb71e0287070062d1a85f21b2689fdf56d7
M. Thoonen <m.a.w.thoonen@student.utwente.nl>
2021-01-14 22:52:34 +0100

    qutoeshite

--- addquote.php
+++ addquote.php
@@ -1,10 +1,15 @@
 <?php
 echo "addquote"
-$db = mysqli_connect("localhost","huizehigh5_quoteform","m#&AmXk*_S&[","huizehigh5_quotes")or die('Er kan geen verbinding met de database worden gemaakt.');
 
-$sql = mysqli_query()
+$id = 0;
+$naam = $_POST["naam"];
+$quote = $_POST["quote"];
+$db = mysqli_connect("localhost","huizehigh5_quoteform","~6ClA9s^)NjW","huizehigh5_quotes")or die('Er kan geen verbinding met de database worden gemaakt.');
+
+$sql = mysqli_query($db,"INSERT INTO quotes (ID,naam,quote,tijd) VALUES ('$id','$naam','$quote','".time()."')");
 
 echo "succes!"
+echo '<meta http-equiv=REFRESH CONTENT=0;url='huizehighfive.calslaan.nl/quotes'>';
 ?>
 <!--?
 echo '<meta http-equiv=REFRESH CONTENT=0;url='huizehighfive.calslaan.nl/quotes'>';?-->
