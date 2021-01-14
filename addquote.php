dbd08ebb1a05d316596a72b0a4e3bfd530d72286
M. Thoonen <m.a.w.thoonen@student.utwente.nl>
2021-01-14 23:01:55 +0100

    asdf

--- addquote.php
+++ addquote.php
@@ -1,5 +1,5 @@
 <?php
-echo "addquote"
+echo "addquote";
 
 $id = 0;
 $naam = $_POST["naam"];
@@ -8,7 +8,7 @@ $db = mysqli_connect("localhost","huizehigh5_quoteform","~6ClA9s^)NjW","huizehig
 
 $sql = mysqli_query($db,"INSERT INTO quotes (ID,naam,quote,tijd) VALUES ('$id','$naam','$quote','".time()."')");
 
-echo "succes!"
+echo "succes!";
 echo '<meta http-equiv=REFRESH CONTENT=0;url='huizehighfive.calslaan.nl/quotes'>';
 ?>
 <!--?
