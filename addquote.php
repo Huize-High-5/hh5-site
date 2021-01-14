c8ff0c169586795add7096aefabe3a1535170708
M. Thoonen <m.a.w.thoonen@student.utwente.nl>
2021-01-14 23:06:25 +0100

    zucht

--- addquote.php
+++ addquote.php
@@ -9,7 +9,5 @@ $db = mysqli_connect("localhost","huizehigh5_quoteform","~6ClA9s^)NjW","huizehig
 $sql = mysqli_query($db,"INSERT INTO quotes (ID,naam,quote,tijd) VALUES ('$id','$naam','$quote','".time()."')");
 
 echo "succes!";
-echo '<meta http-equiv=REFRESH CONTENT=0;url='huizehighfive.calslaan.nl/quotes'>';
+echo "<meta http-equiv=REFRESH CONTENT=0;url='huizehighfive.calslaan.nl/quotes'>";
 ?>
-<!--?
-echo '<meta http-equiv=REFRESH CONTENT=0;url='huizehighfive.calslaan.nl/quotes'>';?-->
