5290662e7d7e31dedc767501528a3d8449218c3b
M. Thoonen <m.a.w.thoonen@student.utwente.nl>
2021-01-14 22:40:47 +0100

    addquote

--- addquote.php
+++ addquote.php
@@ -1,4 +1,5 @@
 <?php
+echo "addquote"
 $db = mysqli_connect("localhost","huizehigh5_quoteform","m#&AmXk*_S&[","huizehigh5_quotes")or die('Er kan geen verbinding met de database worden gemaakt.');
 
 $sql = mysqli_query()
