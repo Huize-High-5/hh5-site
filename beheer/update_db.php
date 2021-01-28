c46d871271ff7a9a7d5d64f9c7466cd5813fdb34
M. Thoonen <m.a.w.thoonen@student.utwente.nl>
2021-01-28 12:43:38 +0100

    asf

--- beheer/update_db.php
+++ beheer/update_db.php
@@ -9,10 +9,18 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-echo $_GET['ID'];
+echo $_GET['Tijd'];
 $sql = "UPDATE quotes SET Zichtbaar = !Zichtbaar WHERE quotes.Tijd = ".$_GET['Tijd'];
 echo $sql;
 $result = $conn->query($sql);
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    die();
+  }
+
 echo '<br />db aangepast <br />';
 echo "<meta http-equiv=REFRESH CONTENT=0;url='index.php'>";
 
